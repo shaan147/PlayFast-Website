7d686ee32789ec78b667bcff18dd9196aa775793
Mujtaba Azhar <mujtaba@quantomex.com>
2024-02-25 01:54:14 +0500

    UserPages

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,23 @@
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+// Get the button:
+let mybutton = document.getElementById("back-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
+window.onscroll = function() {
   scrollFunction();
 };
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
+  if (window.scrollY > 20 || window.pageYOffset > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
+
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+mybutton.addEventListener("click", topFunction);
 
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-
