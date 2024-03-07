6364a58e1c1f18dbbfd03c97f3ec03bdc26f9532
MujtabaAzhar-1002 <mujtaba@quantomex.com>
2024-03-07 22:34:47 +0500

    90%Responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,25 @@
-// Get the button:
-let mybutton = document.getElementById("back-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-  scrollFunction();
-};
 
-function scrollFunction() {
-  if (window.scrollY > 20 || window.pageYOffset > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", topFunction);
 
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
 
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
 
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", function () {
